b0b222fb16e1f48881bdd20bb7e53223dde5727b
Lincoln Barsotti <lincolngabriel19@gmail.com>
2024-03-30 23:09:22 -0300

    Initial project setup

---
+++ src/test/java/br/com/petspot/PetSpotApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.petspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetSpotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
