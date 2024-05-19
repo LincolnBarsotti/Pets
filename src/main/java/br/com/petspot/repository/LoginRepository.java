ab1ef739622ea408e017ad48104fb96efe403f04
Lincoln Barsotti <107706237+LincolnBarsotti@users.noreply.github.com>
2024-05-18 23:00:15 -0300

    :goal_net: feat: Email authentication before register

--- src/main/java/br/com/petspot/repository/LoginRepository.java
+++ src/main/java/br/com/petspot/repository/LoginRepository.java
@@ -10,4 +10,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface LoginRepository extends JpaRepository<Login, String> {
     Login findByEmailAndPasswordLogin(String email, String senha);
 
+    boolean existsLoginByEmail(String email);
 }
