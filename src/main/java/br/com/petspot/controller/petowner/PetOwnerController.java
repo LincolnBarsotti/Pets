cc05f1dd4790d7fb55992e976700a191b14d200d
Lincoln Barsotti <107706237+LincolnBarsotti@users.noreply.github.com>
2024-05-21 23:54:41 -0300

    :recycle: refactor: Changed Type of User to Enum Class

--- src/main/java/br/com/petspot/controller/petowner/PetOwnerController.java
+++ src/main/java/br/com/petspot/controller/petowner/PetOwnerController.java
@@ -1,38 +1,9 @@
 package br.com.petspot.controller.petowner;
 
-import br.com.petspot.model.dto.logindto.LoginDto;
-import br.com.petspot.model.dto.logindto.RequestEmailDto;
-import br.com.petspot.model.dto.petowner.registerdto.RegisterDto;
-import br.com.petspot.model.messages.login.MessageToRequestNewPassword;
-import br.com.petspot.service.petowner.PetOwnerService;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 public class PetOwnerController {
-    @Autowired
-    private PetOwnerService petOwnerService;
 
-    @PostMapping
-    @RequestMapping("/register")
-    private ResponseEntity register(@RequestBody @Validated RegisterDto registerDto, UriComponentsBuilder uriBuilder){
-        return petOwnerService.register(registerDto, uriBuilder);
-    }
-
-    @PostMapping
-    @RequestMapping("/login")
-    public ResponseEntity signIn(@RequestBody @Valid LoginDto loginDto) {
-        return petOwnerService.signIn(loginDto);
-    }
-
-    @PutMapping
-    @RequestMapping("/request-new-password")
-    public ResponseEntity<MessageToRequestNewPassword> requestNewPassword(@RequestBody @Valid RequestEmailDto emailDto){
-        return petOwnerService.recoverPassword(emailDto.email());
-    }
 
 }
--- src/main/java/br/com/petspot/service/petowner/PetOwnerService.java
+++ src/main/java/br/com/petspot/service/petowner/PetOwnerService.java
@@ -1,69 +1,9 @@
 package br.com.petspot.service.petowner;
 
-import br.com.petspot.model.dto.logindto.LoginDto;
-import br.com.petspot.model.messages.login.MessageLoginDto;
-import br.com.petspot.model.messages.login.MessageToRequestNewPassword;
-import br.com.petspot.model.messages.register.MessageResgiterDto;
-import br.com.petspot.model.dto.petowner.registerdto.RegisterDto;
-import br.com.petspot.model.entity.login.Login;
-import br.com.petspot.model.entity.petOwner.PetOwner;
-import br.com.petspot.repository.LoginRepository;
-import br.com.petspot.service.email.SendEmail;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 public class PetOwnerService {
-
-    @Autowired
-    private LoginRepository loginRepository;
-
-    @Autowired
-    private SendEmail sendEmail;
-
-    public ResponseEntity signIn(LoginDto loginDto){
-        Login auth = loginRepository.findByEmailAndPasswordLogin(loginDto.email(), loginDto.senha());
-
-        if (auth != null){
-            sendEmail.sendEmail(auth.getEmail(),
-                    "Login efetuado",
-                    "Login efetuado em nossa plataforma");
-            return ResponseEntity.ok(new MessageLoginDto(loginDto.email()));
-        }
-
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas!");
-    }
-
-    public ResponseEntity register(RegisterDto registerDto, UriComponentsBuilder uriBuilder){
-
-        if (loginRepository.existsLoginByEmail(registerDto.email())){
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Outra conta está usando o mesmo email.");
-        }
-
-        Login login = new Login(registerDto);
-        PetOwner petOwner = new PetOwner(registerDto);
-
-        login.setPetOwner(petOwner);
-
-        loginRepository.save(login);
-
-        var uri = uriBuilder.path("/profile/{id}").buildAndExpand(login.getId()).toUri();
-
-        return ResponseEntity.created(uri).body(new MessageResgiterDto(login.getEmail()));
-    }
-
-    public ResponseEntity<MessageToRequestNewPassword> recoverPassword(String email){
-        if (loginRepository.existsLoginByEmail(email)){
-            sendEmail.sendEmail(email, "Recuperação de senha","Houve um pedido de solicitação de senha, por favor acesse o link:\n link");
-            return ResponseEntity.ok(new MessageToRequestNewPassword("Email enviado com sucesso"));
-        }
-
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new MessageToRequestNewPassword("Verifique se o email foi digitado corretamente"));
-    }
-
 
 
 }
