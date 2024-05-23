b20e1ef754c7416ef3c03705e9d9e320fdaae1a0
Lincoln Barsotti <107706237+LincolnBarsotti@users.noreply.github.com>
2024-05-22 23:39:53 -0300

    :bug: fix: Password Max and Min value

--- src/main/java/br/com/petspot/model/dto/logindto/NewPasswordDto.java
+++ src/main/java/br/com/petspot/model/dto/logindto/NewPasswordDto.java
@@ -1,8 +1,6 @@
 package br.com.petspot.model.dto.logindto;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Pattern;
 
 public record NewPasswordDto(
@@ -10,8 +8,6 @@ public record NewPasswordDto(
     String email,
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[~!@#$%^&*()_+{}|;':\"/.,<>?])[a-zA-Z0-9~!@#$%^&*()_+{}|;':\"/.,<>?]+$",
             message = "Senha inválida!")
-    @Min(value = 8, message = "Senha precisa ter no mínimo 8 caracteres")
-    @Max(value = 23, message = "Senha precisa ter no máximo 23 caracteres")
     String senha
 ){
 
--- src/main/java/br/com/petspot/model/dto/logindto/RegisterUserDto.java
+++ src/main/java/br/com/petspot/model/dto/logindto/RegisterUserDto.java
@@ -11,8 +11,6 @@ public record RegisterUserDto(
         String email,
         @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[~!@#$%^&*()_+{}|;':\"/.,<>?])[a-zA-Z0-9~!@#$%^&*()_+{}|;':\"/.,<>?]+$",
                 message = "Senha inválida!")
-                @Min(value = 8, message = "Senha precisa ter no mínimo 8 caracteres")
-                @Max(value = 23, message = "Senha precisa ter no máximo 23 caracteres")
         String senha,
         @NotBlank
         String nome,
