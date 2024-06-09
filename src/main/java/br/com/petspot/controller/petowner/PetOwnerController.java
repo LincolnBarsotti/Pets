8b74ee93ba6b6c60a98f612be90e2ea5ef7405f5
Lincoln Barsotti <107706237+LincolnBarsotti@users.noreply.github.com>
2024-06-08 23:10:52 -0300

    :construction: feat: Pet History

--- src/main/java/br/com/petspot/controller/petowner/PetOwnerController.java
+++ src/main/java/br/com/petspot/controller/petowner/PetOwnerController.java
@@ -1,9 +1,19 @@
 package br.com.petspot.controller.petowner;
 
+import br.com.petspot.service.petowner.PetOwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("{id}")
 public class PetOwnerController {
 
+    @Autowired
+    private PetOwnerService petOwnerService;
 
+    @GetMapping("/feed")
+    public ResponseEntity getFeed(@PathVariable(name = "id") String tutorID){
+        return ResponseEntity.ok(petOwnerService.getFeed(tutorID));
+    }
 }
--- src/main/java/br/com/petspot/service/petowner/PetOwnerService.java
+++ src/main/java/br/com/petspot/service/petowner/PetOwnerService.java
@@ -1,9 +1,23 @@
 package br.com.petspot.service.petowner;
 
+import br.com.petspot.model.entity.petOwner.PetOwner;
+import br.com.petspot.repository.PetOwnerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PetOwnerService {
 
+    @Autowired
+    private PetOwnerRepository petOwnerRepository;
 
+    public ResponseEntity getFeed(String tutorID) {
+        PetOwner tutor = petOwnerRepository.getReferenceById(tutorID);
+        if (tutor.getPet().isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+        return null;
+    }
 }
