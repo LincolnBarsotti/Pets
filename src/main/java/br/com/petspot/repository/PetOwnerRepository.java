7be95b497c4e725d6236b3c8228c154e75d011cb
Lincoln Barsotti <107706237+LincolnBarsotti@users.noreply.github.com>
2024-06-12 20:29:47 -0300

    :construction: refactor: Repositories updated

--- src/main/java/br/com/petspot/repository/PetOwnerRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.petspot.repository;
-
-import br.com.petspot.model.entity.petOwner.PetOwner;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PetOwnerRepository extends JpaRepository<PetOwner, String> {
-}
