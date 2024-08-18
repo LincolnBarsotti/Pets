1440d6a4d8e9a979b4e6aa8e0e0e604dafcbe5a1
Lincoln Barsotti <107706237+LincolnBarsotti@users.noreply.github.com>
2024-08-17 21:46:37 -0300

    :construction: refactor: SQL vaccine and history

--- src/main/resources/db/migration/V5__create-table-service-history.sql
+++ src/main/resources/db/migration/V5__create-table-service-history.sql
@@ -1,15 +1,7 @@
 create table service_history(
     service_id VARCHAR(36) PRIMARY KEY,
     name_service VARCHAR(255),
-    creation_date DATE
+    creation_date DATE,
+    pet_id VARCHAR(36),
+    FOREIGN KEY (pet_id) REFERENCES pet(id)
 );
-
-create table pets_tutor_history_service(
-  id int AUTO_INCREMENT primary key,
-  pet_id VARCHAR(36),
-  tutor_id VARCHAR(36),
-  service_history_id VARCHAR(36),
-  FOREIGN KEY (pet_id) REFERENCES pet(id),
-  FOREIGN KEY (tutor_id) REFERENCES person(id_person),
-  FOREIGN KEY (service_history_id) REFERENCES service_history(service_id)
-)
--- src/main/resources/db/migration/V6__create-table-vaccine.sql
+++ src/main/resources/db/migration/V6__create-table-vaccine.sql
@@ -1,10 +1,9 @@
 CREATE TABLE vaccine_registration (
      id_vaccine INT PRIMARY KEY AUTO_INCREMENT,
-     name_vaccine VARCHAR(255) NOT NULL,
-     name_who_registered_vaccine VARCHAR(255) NOT NULL,
-     registration_date_vaccine DATE NOT NULL,
-     vaccine_function VARCHAR(255) NOT NULL,
-     vaccine_batch VARCHAR(255) NOT NULL,
-     vaccine_manufacturer VARCHAR(255) NOT NULL
-
+     name_vaccine VARCHAR(255) ,
+     name_who_registered_vaccine VARCHAR(255) ,
+     registration_date_vaccine DATE ,
+     vaccine_function VARCHAR(255) ,
+     vaccine_batch VARCHAR(255) ,
+     vaccine_manufacturer VARCHAR(255)
 );
